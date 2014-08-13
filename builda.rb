2338ea24a32ef2b3616139d20419e75be9c77fc6
Arfon Smith <arfon@github.com>
2014-08-12 18:08:18 -0700

    More template code

---
+++ builda.rb
@@ -0,0 +1,47 @@
+%w{rubygems sinatra mongo_mapper digest pry multi_json octokit sidekiq oauth}.each { |dep| require dep }
+
+if ENV['MONGOHQ_URL']
+  uri = URI.parse(ENV['MONGOHQ_URL'])
+  MongoMapper.connection = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+  MongoMapper.database = uri.path.gsub(/^\//, '')
+else
+  MongoMapper.setup({"development" => { "host" => "localhost", "database" => "builda_development", "port" => 27017}}, 'development')
+end
+
+
+class Repository
+  include MongoMapper::Document
+
+  key :name, String
+  key :secret, String
+  key :github_address, String
+  timestamps!
+
+  many :build_requests
+end
+
+class BuildRequest
+  include MongoMapper::EmbeddedDocument
+
+  key :commit, String
+  key :status, String
+  timestamps!
+end
+
+class BuildaWorker
+  include Sidekiq::Worker
+
+  def perform(repository_id, build_request_id)
+    "DO MAGIC"
+  end
+end
+
+before do
+  @repository = Repository.find_by_secret(params[:secret])
+  return status 404 unless @repository
+end
+
+post '/build' do
+  # Call BuildaWorker here e.g. BuildaWorker.perform_async(@repository.id, @build_request.id)
+  "BUILDAAAAAAAA!"
+end
